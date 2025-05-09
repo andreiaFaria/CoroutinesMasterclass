f537c343fc0a54a1b078d249ea94eefd3367acc4
andreiafaria <andreia.faria@ctw.bmwgroup.com>
2025-05-09 10:01:00 +0100

    - Assignment 1

--- app/src/main/java/com/plcoding/coroutinesmasterclass/util/RotatingBoxScreen.kt
+++ app/src/main/java/com/plcoding/coroutinesmasterclass/util/RotatingBoxScreen.kt
@@ -9,12 +9,16 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun RotatingBoxScreen(modifier: Modifier = Modifier) {
@@ -27,6 +31,31 @@ fun RotatingBoxScreen(modifier: Modifier = Modifier) {
         ),
         label = ""
     )
+
+    val scope = rememberCoroutineScope()
+
+    LaunchedEffect(Unit) {
+        scope.launch {
+            repeat(4){
+                println("Coo")
+                delay(1000)
+            }
+        }
+        scope.launch {
+            repeat(4){
+                println("Caw")
+                delay(2000)
+            }
+        }
+        scope.launch {
+            repeat(4){
+                println("Chirp")
+                delay(3000)
+            }
+        }
+    }
+
+
     Box(
         modifier = Modifier
             .fillMaxSize(),
